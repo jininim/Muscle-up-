691b9cdf56be622ce394d0f7e1ae00037c06c2c8
developer Jin <91578450+jininim@users.noreply.github.com>
2022-11-21 00:53:42 +0900

    check

--- app/src/main/java/com/example/teamproject_hometrainingassistant_app/MainActivity.kt
+++ app/src/main/java/com/example/teamproject_hometrainingassistant_app/MainActivity.kt
@@ -12,7 +12,7 @@ import com.example.teamproject_hometrainingassistant_app.databinding.ActivityMai
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-//hi 지원이형
+//hi 지원이형 adsasdasdasdsa
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
