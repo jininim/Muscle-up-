7a0d16aec81a25d94ba32d22fa7a73490907fc20
developer Jin <91578450+jininim@users.noreply.github.com>
2023-01-09 19:04:15 +0900

    홈프래그먼트 xml 수정

--- app/src/main/java/com/example/teamproject_hometrainingassistant_app/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/teamproject_hometrainingassistant_app/ui/home/HomeFragment.kt
@@ -22,15 +22,15 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
+//        val homeViewModel =
+//            ViewModelProvider(this).get(HomeViewModel::class.java)
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+//        val textView: TextView = binding.textHome
+//        homeViewModel.text.observe(viewLifecycleOwner) {
+//            textView.text = it
+//        }
         return binding.root
     }
 
