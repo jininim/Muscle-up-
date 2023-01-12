6f9e8fa0198430a0f51e121ab1a13c8e04ccb0ea
jiwonHub <jiwon31273@naver.com>
2023-01-12 13:59:39 +0900

    dashboard recycler commit

--- app/src/main/java/com/example/teamproject_hometrainingassistant_app/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/teamproject_hometrainingassistant_app/ui/dashboard/DashboardFragment.kt
@@ -1,16 +1,20 @@
 package com.example.teamproject_hometrainingassistant_app.ui.dashboard
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.teamproject_hometrainingassistant_app.R
+import com.example.teamproject_hometrainingassistant_app.VerticalItemDecorator
 import com.example.teamproject_hometrainingassistant_app.databinding.FragmentDashboardBinding
 
 class DashboardFragment : Fragment() {
 
+    lateinit var dashboardAdapter: DashboardAdapter
+    val datas = mutableListOf<DashboardRoutineData>()
     private var _binding: FragmentDashboardBinding? = null
 
     // This property is only valid between onCreateView and
@@ -28,11 +32,30 @@ class DashboardFragment : Fragment() {
         _binding = FragmentDashboardBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+        initRecycler()
+
         return root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    private fun initRecycler(){
+        dashboardAdapter = DashboardAdapter(this)
+        binding.recyclerView.adapter = dashboardAdapter
+        binding.recyclerView.addItemDecoration(VerticalItemDecorator(0))
+
+        datas.apply {
+            add(DashboardRoutineData(img = R.drawable.ic_photo, text = "루틴 1"))
+            add(DashboardRoutineData(img = R.drawable.ic_photo, text = "루틴 2"))
+            add(DashboardRoutineData(img = R.drawable.ic_photo, text = "루틴 3"))
+
+            dashboardAdapter.datas = datas
+            dashboardAdapter.notifyDataSetChanged()
+        }
+
     }
 }
---
+++ app/src/main/java/com/example/teamproject_hometrainingassistant_app/ui/dashboard/DashboardRoutineData.kt
@@ -0,0 +1,6 @@
+package com.example.teamproject_hometrainingassistant_app.ui.dashboard
+
+data class DashboardRoutineData(
+    val img : Int,
+    val text : String
+)
